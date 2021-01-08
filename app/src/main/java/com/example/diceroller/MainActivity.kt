c89085e1ae8bd3e838f515ffd8e363c7b9c8de43
kastrahl <kastrahl@users.noreply.github.com>
2021-01-08 23:42:38 +0530

    added image view to the app and dice images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -26,14 +27,34 @@ class MainActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById(R.id.Rollednumber)
         resultTextView.text = diceRoll.toString()
         val luckyTextView: TextView = findViewById(R.id.lucky_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
         when (diceRoll) {
             luckyNumber -> luckyTextView.text = ("You won!")
-            1 -> luckyTextView.text = ("So sorry! You rolled a 1. Try again!")
-            2 -> luckyTextView.text = ("Sadly, you rolled a 2. Try again!")
-            3 -> luckyTextView.text = ("Unfortunately, you rolled a 3. Try again!")
-            4 -> luckyTextView.text = ("No luck! You rolled a 4. Try again!")
-            5 -> luckyTextView.text = ("Don't cry! You rolled a 5. Try again!")
-            6 -> luckyTextView.text = ("Apologies! you rolled a 6. Try again!")
+            1 -> {
+                luckyTextView.text = ("So sorry! You rolled a 1. Try again!")
+                diceImage.setImageResource(R.drawable.dice_1)
+            }
+            2 -> {
+                diceImage.setImageResource(R.drawable.dice_2)
+                luckyTextView.text = ("Sadly, you rolled a 2. Try again!")
+            }
+            3 -> {
+                diceImage.setImageResource(R.drawable.dice_3)
+                luckyTextView.text = ("You rolled a 3. Try again!")
+            }
+            4 -> {
+                diceImage.setImageResource(R.drawable.dice_4)
+                luckyTextView.text = ("No luck! You rolled a 4. Try again!")
+            }
+            5 -> {
+                diceImage.setImageResource(R.drawable.dice_5)
+                luckyTextView.text = ("Don't cry! You rolled a 5. Try again!")
+            }
+            6 -> {
+                diceImage.setImageResource(R.drawable.dice_6)
+                luckyTextView.text = ("Apologies! you rolled a 6. Try again!")
+            }
         }
     }
 }
